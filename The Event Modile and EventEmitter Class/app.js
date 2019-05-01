87b3794283d1988f7ae2bd94d70a3c7f7fd35928
TaimoorSaeed <taimoor.dev@gmail.com>
2019-05-01 18:39:16 +0500

    Event module and event emitter class

---
+++ The Event Modile and EventEmitter Class/app.js
@@ -0,0 +1,12 @@
+const tutorial = require('./tutorial')
+
+console.log(tutorial.sum(1,2))
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathsObject());
+// console.log(tutorial(1,1))
+
+
+
+
+
+
